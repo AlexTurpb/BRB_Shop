3bbffdf9b30a7a9fa1c605e2e9567e7d62f28519
Alex_Turpb <turpb41@gmail.com>
2018-03-22 23:50:57 +0200

    Barber check logic and db seed as in L27 spep 4

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where barber_name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (barber_name) values (?)', [barber]
+		end
+	end
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
   	db.results_as_hash = true	
@@ -29,9 +41,12 @@ configure do
 	  		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 	  		"barber_name" TEXT UNIQUE
 	  	)'
-	get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Walter White'
-	get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Jessie Pinkman'
-	get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Gus Fring'
+
+	seed_db get_db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Fourth Barber']
+
+	# get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Walter White'
+	# get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Jessie Pinkman'
+	# get_db.execute "insert or ignore into Barbers(barber_name) values( ?)", 'Gus Fring'
 	end
 end
 
